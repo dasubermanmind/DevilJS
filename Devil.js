202ad919bc73bd60735a7575031dd9dc1e42d7a0
EOstling <it.treugott@gmail.com>
2022-04-21 09:56:16 -0400

    Added Block scope, variable declarations, Immutability

--- Devil.js
+++ Devil.js
@@ -3,24 +3,16 @@
 * AST Interpreter
 * */
 
-
-//TODO: the Devil Parser will be imported here
-//const fs = require('fs');
 const assert = require('assert');
-
-const {isNumberObject} = require('util/types');
+const Enviroment = require('./Environment');
 
 class Devil {
 
-    // TODO: Define Global Enviroment, these will define what operators the devil programmer can use
-    constructor(global) {
-        this.global = global;
-        //TODO: create a new Transormer object
-
+     constructor(global = new Enviroment()) {
+         this.global = global;
     }
 
-    // TODO: Eventually these will be in a helper class
-    eval(exp) {
+    eval(exp, env=this.global) {
         if(isNumber(exp)) {
             return exp;
         }
@@ -29,28 +21,62 @@ class Devil {
             return exp.slice(1,-1);
         }
 
-        // this in inlined for now
-        // but will live in environment
-        // This handles simple case, but embedded (ie expression + expression)+expressiom 
         if(exp[0] === '+') {
-            // this handles both cases
             return this.eval(exp[1]) + this.eval(exp[2]);
         }
 
-        throw 'Not implemented yet. The Devil can only work so fast. Please be patient.';
-    }
 
-    
-    _eval(body, env=this.global) {
-        //TODO: This will evaluate each expression in a specific environment | Defaults to Global
-
-    }
-
-    _evalBody (body, env) {
-        if (body[0] == 'begin') { 
-            return this._evalBlock(body,env);
+        if (exp[0] === '*') {
+            return this.eval(exp[1]) * this.eval(exp[2]);
         }
-        return this.eval(body,env);
+
+
+        if (exp[0] === '/') {
+            return this.eval(exp[1] / this.eval(exp[2]));
+        }
+
+        // to create a variable we go with the pattern of (dev x 0)
+        // By default All variables are immutable
+        // We must add functionality to make it mutable 
+        if( exp[0] === 'dev') {
+            const [_, name, value] = exp;
+            return Object.freeze(env.define(name, value));
+        }
+
+        // TODO: Mutable Case
+        // We can define a variable as mutable by using the keyword `mut`
+        // (dev mut x 0)
+        if( exp[0] === 'dev' && exp[1] === 'mut') {
+            const [_, name, value] = exp;
+            return env.define(name,value);
+        }
+
+        if (isVariableName(exp)) {
+            return env.lookup(exp);
+        }
+
+        // Now we begin by thinking of block scope, a sequence of expressions
+        if (exp[0] === 'begin') {
+            return this._evalBlock(exp,env);
+        }
+
+        throw `Not implemented yet. The Devil can only work so fast. Please be patient. Issue is = ${JSON.stringify(exp)}`;
+    }
+
+    // We care about scope which block it is in
+    _evalBlock(block, env) {
+        let result;
+
+        const [_, tag, ...expressions] = block;
+        console.log('tag', tag);
+        console.log('expressions', expressions);
+
+        expressions.forEach(exp => {
+            result = this.eval(exp,env);
+        });
+
+        console.log(result);
+        return result;
     }
 }
 
@@ -58,22 +84,37 @@ function isNumber(exp) {
     return typeof exp === 'number';
 }
 
-
 function isString(exp) {
     return typeof exp === 'string' && exp[0].slice(-1) === '"';
 
 }
 
+function isVariableName(exp) {
+    return typeof exp === 'string' && /^[a-zA-Z][a-zA-Z0-9_]*$/.test(exp);
+}
+
 
 // TODO: Move these to its own test dir
-// For now this is fine for local dev
-
 const devil = new Devil();
 
 assert.strictEqual(devil.eval(['+', 1, 5]), 6);
 assert.strictEqual(devil.eval('"TEST"'), 'TEST')
 assert.strictEqual(devil.eval(['+', ['+', 3, 2], 5]), 10);
 
-console.log('In Theory all these tests have passed if you see this');
+assert.strictEqual(devil.eval(['dev','y',100]), 100);
+assert.strictEqual(devil.eval(['dev', 'x',10]), 10);
 
+/*
+assert.strictEqual(devil.eval(
+    ['begin',
+
+      ['dev', 'x', 10],
+      ['dev', 'y', 20],
+
+      ['+', ['*', 'x', 'y'], 30],
+
+    ]),
+  230);
+*/
+console.log('In Theory all these tests have passed if you see this');
 module.exports = Devil;
--- Environment.js
+++ Environment.js
@@ -2,14 +2,12 @@
  * Base class for Environments of DevilsJS
  * */
 
-
 // TODO: The API of the environment is as follows: 
 // 1) Create a variable with a (name, value) \
 // **Note: The value is immutable by default
 // 2) If defined as mutable, Update the existing variable
 // 3) Return the value of a defined variable or throw error if not created within the correct scope
 // 4) Return the Specific environment in which a variable is declared in
-
 
 class Environments {
 
@@ -29,13 +27,14 @@ class Environments {
     }
 
     lookup(name) {
-        return this.resolve(name).record[name];
+        if(!this.record.hasOwnProperty(name)){
+            throw new ReferenceError(`Variable ${name} is not defined`);
+        }
+        return this.record[name];
+       //return this.resolve(name).record[name];
     }
 
-
-    resolve(name) {
-        // TODO: Implement this
-    }
+    resolve(name) {}
 
 
 }
